542d39edea5d4400906fccd4bd4432605884769c
sangjin <sangjinj7@gmail.com>
2016-07-18 09:50:42 +0900

    이건 테스트 수정

--- app/src/main/java/webview/sangjinj/notipush/notipushActivity.java
+++ app/src/main/java/webview/sangjinj/notipush/notipushActivity.java
@@ -2,6 +2,7 @@ package webview.sangjinj.notipush;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
 
 public class notipushActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class notipushActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notipush);
+
+
+        Log.d("-----","");
+        Log.d("-----","");
+        Log.d("-----","");
     }
 }
